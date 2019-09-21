d08c31e465ed65f9386fafd6574a2fb8a0c3623e
dalmabot <49755674+dalmabot@users.noreply.github.com>
2019-09-21 16:07:28 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const PREFIX = ":";
+
+bot.on("guildMemberAdd", user =>{
+    let joinEmbed = new Discord.RichEmbed()
+        .setColor("#00dbfb")
+        .setAuthor(user.user.username, user.user.displayAvatarURL)
+        .setDescription(":grin: Bienvenue" + user + "sur le serveur " + user.guild.name + " !"
+        )
+        .setFooter("Creer par Dalmatheo")
+    user.guild.channels.get("434747726157053982").send(joinEmbed)
+    user.user.createDM().then()
+    user.addRole("434746554130104320")
+});
+
+bot.on("guildMemberRemove", user =>{
+    let leaveEmbed = new Discord.RichEmbed()
+        .setColor("#00dbfb")
+        .setAuthor(user.user.username, user.user.displayAvatarURL)
+        .setDescription("Sniff " + user.user.username + "est partie ...")
+        .setFooter("Crée par Dalmatheo")
+    user.guild.channels.get("434747726157053982").send(leaveEmbed)
+});
+
+bot.on('message', message => {
+    if(message.content[0] === PREFIX) {
+        if(message.content === ":help") {
+            message.channel.send('Je peut t aidé');
+        }
+    }
+    if(message.content[0] === PREFIX) {
+        if(message.content === ":Mes infos") {
+            message.channel.send('je peut le faire, ton client :' + message.author.client);
+        }
+    }
+});
+
+bot.on("message", message => {
+
+    bot.on("guildMemberRemove", user =>{
+        user.createDM().then(channel => {
+            channel.send("test");
+        })
+    });
+    })
+
+bot.login('NjI0MjcwMjgyNTQyMDIyNjU5.XYYnTw.5CcNoes2TivH-Ntb9oOJTdPBg5Y')
